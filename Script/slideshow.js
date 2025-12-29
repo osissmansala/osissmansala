5827cc172a35bbfbfe0f3a1330c6295cc1b84fd7
Zafranaqeel <zafranaqeel62@gmail.com>
2025-12-29 21:27:58 +0700

    launching v1.0

---
+++ Script/slideshow.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 15000); // ganti slide tiap 15 detik
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
